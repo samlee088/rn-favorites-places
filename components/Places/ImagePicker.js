d20ab91b2df36d128a126436036cbdb8fde0ec88
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-09-04 10:44:09 -0700

    able to grant device permissions

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,10 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Button } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import {
+  PermissionStatus,
+  launchCameraAsync,
+  useCameraPermissions,
+} from "expo-image-picker";
+import { Alert } from "react-native";
 
 const ImagePicker = () => {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to utilize this app"
+      );
+      return false;
+    }
+
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
